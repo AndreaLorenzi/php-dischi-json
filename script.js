81f32d8286ca7cfc34eb03b97674cfe94d064db2
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-06-09 16:50:28 +0200

    portato arry in php tramite axios

--- script.js
+++ script.js
@@ -3,49 +3,17 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      albums: [
-        {
-          img: "img/NewJersey.jpg",
-          title: "New Jersey",
-          author: "Bon jovi",
-          year: "1988",
-        },
-
-        {
-          img: "img/LiveatWembley.jpg",
-          title: "Live at Wembley 86",
-          author: "Queen",
-          year: "1992",
-        },
-
-        {
-          img: "img/Summoner.jpg",
-          title: "Ten's Summoner's Tales",
-          author: "Sting",
-          year: "1993",
-        },
-
-        {
-          img: "img/Stevegaddband.jpg",
-          title: "Steve Gadd band",
-          author: "Steve Gadd Band",
-          year: "2018",
-        },
-
-        {
-          img: "img/bravenewworld.jpg",
-          title: "Brave new world",
-          author: "Iron Maiden",
-          year: "2000",
-        },
-
-        {
-          img: "img/Onemorecar.jpg",
-          title: "One more car, one more rider",
-          author: "Eric Clapton",
-          year: "2002",
-        },
-      ],
+      albums: [],
     };
+  },
+  methods: {
+    requestAlbum() {
+      axios
+        .get("http://localhost:8888/php-dischi-json/server.php")
+        .then((Response) => (this.albums = Response.data));
+    },
+  },
+  created() {
+    this.requestAlbum();
   },
 }).mount("#app");
